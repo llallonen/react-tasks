cd81433fcde11b8f73025617819fb912e806bd25
llallonen <llallonen@gmail.com>
2023-03-17 23:14:32 +0400

    feat: add router

---
+++ 01-components/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import { About } from './pages/About';
+import { Main } from './pages/Main';
+import { NotFound } from './pages/NotFound';
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </header>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='about' element={<About />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App;
---
+++ 01-components/src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App'
+import './index.css'
+import { About } from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
