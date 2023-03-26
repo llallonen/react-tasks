b75d2eb3ce72fcf0bd334110b9beeb58ca370095
llallonen <llallonen@gmail.com>
2023-03-26 19:22:06 +0300

    feat: add select component

---
+++ app/src/components/Form/Select.tsx
@@ -0,0 +1,40 @@
+import React, { RefObject } from 'react';
+export const RegionNames = [
+  'Kanto',
+  'Johno',
+  'Hoenn',
+  'Sinnoh',
+  'Unova',
+  'Kalos',
+  'Alola',
+  'Galar',
+  'Hisui',
+  'Paldea',
+];
+
+type RegionNamesTuple = typeof RegionNames;
+export type Region = RegionNamesTuple[number];
+
+interface SelectProps {
+  selectRef: RefObject<HTMLInputElement>;
+}
+
+class Select extends React.Component<SelectProps> {
+  constructor(props: SelectProps) {
+    super(props);
+  }
+  render() {
+    return (
+      <label>
+        Choose pokemon's region
+        <select>
+          {RegionNames.map((region: Region) => (
+            <option value={region}>{region}</option>
+          ))}
+        </select>
+      </label>
+    );
+  }
+}
+
+export default Select;
