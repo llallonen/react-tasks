1244df3a2501d6e0deed2378d5d0670d0b6b8062
llallonen <llallonen@gmail.com>
2023-04-02 21:53:31 +0500

    feat: turn Pokemon into functional component

--- app/src/components/Pokemon.tsx
+++ app/src/components/Pokemon.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export interface PokemonProps {
   img: string;
   id: number;
@@ -7,18 +5,12 @@ export interface PokemonProps {
   typeList: Array<string>;
 }
 
-export class Pokemon extends React.Component<PokemonProps> {
-  constructor(props: PokemonProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="pokemon">
-        <img className="pokemon__img" src={this.props.img}></img>
-        <span className="pokemon__id">{this.props.id}</span>
-        <h3 className="pokemon__title">{this.props.name}</h3>
-        <p className="pokemon__type">{this.props.typeList.join(', ')}</p>
-      </div>
-    );
-  }
+export function Pokemon(props: PokemonProps) {
+  return (
+    <div className="pokemon">
+      <img className="pokemon__img" src={props.img}></img>
+      <span className="pokemon__id">{props.id}</span>
+      <h3 className="pokemon__title">{props.name}</h3>
+    </div>
+  );
 }
