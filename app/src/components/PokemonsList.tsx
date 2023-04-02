cdbdc03824ff869df2df08d574010fc297e2638e
llallonen <llallonen@gmail.com>
2023-04-02 22:14:54 +0500

    feat: turn PokemonList into functional component

--- app/src/components/PokemonsList.tsx
+++ app/src/components/PokemonsList.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import { Pokemon, PokemonProps } from './Pokemon';
 
 interface PokemonsListProps {
   pokemons?: Array<PokemonProps>;
 }
 
-class PokemonsList extends React.Component<PokemonsListProps> {
-  constructor(props: PokemonsListProps) {
-    super(props);
-  }
-  render() {
-    return !this.props.pokemons
-      ? 'No pokemons :('
-      : this.props.pokemons.map((pokemon) => <Pokemon key={pokemon.id} {...pokemon} />);
-  }
+function PokemonsList(props: PokemonsListProps): JSX.Element {
+  return (
+    <>
+      {!props.pokemons ? (
+        <div>{`No any pokemons :(`}</div>
+      ) : (
+        props.pokemons.map((pokemon) => <Pokemon key={pokemon.id} {...pokemon} />)
+      )}
+    </>
+  );
 }
 
 export default PokemonsList;
