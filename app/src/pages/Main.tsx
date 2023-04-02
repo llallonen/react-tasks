c5fe30fa20f07f6160533dd6559461f2e226fc8f
llallonen <llallonen@gmail.com>
2023-04-03 00:26:41 +0500

    feat: rewrite the main page with the hooks

--- app/src/pages/Main.tsx
+++ app/src/pages/Main.tsx
@@ -1,41 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
 import Layout from '../components/Layout';
-import { PokemonProps } from '../components/Pokemon';
 import PokemonsList from '../components/PokemonsList';
 import SearchBar from '../components/SearchBar';
+import useFetch from '../hooks/useFetch';
 import { POKEMONS_URL } from '../variables';
 
-class Main extends React.Component<
-  object,
-  { searchBarValue: string; pokemons?: Array<PokemonProps> }
-> {
-  constructor(props: object) {
-    super(props);
-    this.state = { searchBarValue: '', pokemons: undefined };
+function Main() {
+  const [pokemonsState, setPokemonsState] = useState('');
+
+  const [data, setData] = useState(null);
+
+  const dataPokemons = useFetch(POKEMONS_URL);
+  //console.log(dataPokemons);
+
+  let currPage = window.location.pathname.slice(1);
+  if (currPage.length === 0) {
+    currPage = 'Home page';
   }
 
-  componentDidMount(): void {
-    fetch(POKEMONS_URL)
-      .then((res) => res.json())
-      .then((pokemons) => {
-        console.log('pokemons', pokemons);
-        this.setState({ pokemons: pokemons.pokemons });
-      });
-  }
-
-  render() {
-    let currPage = window.location.pathname.slice(1);
-    if (currPage.length === 0) {
-      currPage = 'Home page';
-    }
-
-    return (
-      <Layout currentPage={currPage}>
-        <SearchBar />
-        <PokemonsList pokemons={this.state.pokemons} />
-      </Layout>
-    );
-  }
+  return (
+    <Layout currentPage={currPage}>
+      <SearchBar />
+      <PokemonsList pokemons={dataPokemons ? dataPokemons.pokemons : null} />
+    </Layout>
+  );
 }
 
 export { Main };
